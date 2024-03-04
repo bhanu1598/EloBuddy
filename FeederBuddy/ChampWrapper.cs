882e5d54599dfb833faf066c2b1afa7c35f62917
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2024-03-05 02:52:56 +0530

    update file

---
+++ FeederBuddy/ChampWrapper.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace FeederBuddy
+{
+    using EloBuddy;
+
+    public class ChampWrapper
+    {
+        public string Name { get; set; }
+
+        public List<SpellSlot> SpellSlots { get; set; }
+    }
+}
