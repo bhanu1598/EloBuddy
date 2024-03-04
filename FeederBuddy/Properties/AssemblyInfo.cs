3b07b00c57c7cff3eae465625551c429107d6319
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2024-03-05 02:53:29 +0530

    update file

---
+++ FeederBuddy/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FeederBuddy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FeederBuddy")]
+[assembly: AssemblyCopyright("Copyright © Darakath 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4d7e947-3a10-4c01-871b-a9de1725fc2f")]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
