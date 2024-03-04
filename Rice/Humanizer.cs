ce5123603b2e1690129330dccc2ac5c3127afb27
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2024-03-05 02:53:14 +0530

    update file

---
+++ Rice/Humanizer.cs
@@ -0,0 +1,31 @@
+namespace Rice
+{
+    using EloBuddy.SDK;
+
+    //By 0x0539
+    //From SluttyRyze
+    public class Humanizer
+    {
+        public struct Action
+        {
+            public float Delay { get; set; }
+
+            public float LastTick { get; set; }
+        }
+
+        public static Action General, Spell;
+
+        public static void ChangeDelay(Action action, float nDelay)
+        {
+            action.Delay = nDelay;
+        }
+
+        public static bool CheckDelay(Action action)
+        {
+            if (!(Core.GameTickCount - action.LastTick >= action.Delay)) { return false; }
+            
+            action.LastTick = Core.GameTickCount;
+            return true;
+        }
+    }
+}
